aa1fab4e231d0769a47c9b4bc71067b037bbffe6
Kurund Jalmi <kurund.jalmi@gmail.com>
2017-10-09 17:42:24 +0530

    - add listing page

--- fileuploadlisting/files/views.py
+++ fileuploadlisting/files/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import File
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, world! You are at the files index.")
+
+
+def index(request):
+    file_list = File.objects.all()
+    print(file_list)
+    context = {'file_list': file_list}
+    return render(request, 'files/index.html', context)
