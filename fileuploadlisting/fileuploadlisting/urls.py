a660ec24df852debe0c998c4d40e95db92b5ae5a
Kurund Jalmi <kurund.jalmi@gmail.com>
2017-10-31 14:57:19 +0530

    fix the display of uploded files

--- fileuploadlisting/fileuploadlisting/urls.py
+++ fileuploadlisting/fileuploadlisting/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'^$', include('files.urls')),
+    url(r'^', include('files.urls')),
     url(r'^files/', include('files.urls')),
     url(r'^admin/', admin.site.urls),
 ]
